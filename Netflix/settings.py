e740607a76068303925a01e4b2d0a6a51432ab2b
Eddylucero <luceroeddy8@gmail.com>
2025-06-10 23:10:16 -0500

    subida de mi clave

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('qcz%uf#-mc8tr23od9c0jbl9&&gtdeu2bx@=$zdim=+&gi6x5a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
@@ -84,6 +84,7 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
 
 
 # Password validation
