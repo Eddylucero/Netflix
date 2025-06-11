735d13160483fcfeb84425d6a50c204bd14c59cf
Eddylucero <luceroeddy8@gmail.com>
2025-06-11 08:06:54 -0500

    cambio en la base de datos

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -79,11 +79,9 @@ WSGI_APPLICATION = 'Netflix.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'users.sqlite3'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 
