d010678dfa9afc7a33649efea9bb9dfeefcd6309
Eddylucero <luceroeddy8@gmail.com>
2025-06-10 21:27:06 -0500

    mi clave

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('!@+wacm^6ocr2=83c!v*=l8h$u7x90brz2n0m&xl#e%lay0+rx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
